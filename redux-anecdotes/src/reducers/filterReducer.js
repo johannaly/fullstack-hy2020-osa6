e6abbd6f91dfb1df2ad40df1c80241e8568857f3
johannaly <johanna@lyytinen.org>
2020-11-11 14:56:12 +0200

    anekdootit ja backend 6.13

--- redux-anecdotes/src/reducers/filterReducer.js
+++ redux-anecdotes/src/reducers/filterReducer.js
@@ -3,7 +3,7 @@ const initialstate = [
 ]
 
 export const filterChange = (filter) => {
-    console.log(filter)
+    //console.log(filter)
     return{
         type: 'SET_FILTER',
         data: { filter }
@@ -13,7 +13,7 @@ export const filterChange = (filter) => {
 const filterReducer = (state = initialstate, action) => {
     switch(action.type) {
         case 'SET_FILTER':
-            console.log("setFilter", action.data.filter)
+            //console.log("setFilter", action.data.filter)
             state = action.data.filter
             return state
         default:
